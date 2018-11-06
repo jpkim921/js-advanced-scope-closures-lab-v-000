4b31566f39cec046b7d5dd24d4e1c717c4ae468f
Jayon Kim <jpkim921@gmail.com>
2018-11-06 03:23:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function produceDrivingRange(blockRange){
   return function(startPoint, endPoint){
     let distance = Math.abs(parseInt(endPoint) - parseInt(startPoint));
-
     let difference = distance - blockRange
-    // console.log(difference)
 
     if (difference > 0) {
       return `${difference} blocks out of range`
