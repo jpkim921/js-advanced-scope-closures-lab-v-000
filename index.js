da86204410089325e18933266bcf26a50fcfb776
Jayon Kim <jpkim921@gmail.com>
2018-11-06 03:15:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(blockRange){
   return function(startPoint, endPoint){
     let distance = parseInt(endPoint) - parseInt(startPoint);
     let distanceRange = Math.abs(distance)
-    let difference = distanceRange - blockRange
+    let difference = Math.abs(distanceRange - blockRange)
     // console.log(difference)
 
     if (difference < 0) {
