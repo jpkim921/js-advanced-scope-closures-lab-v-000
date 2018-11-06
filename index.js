f172c84f1464b9452ea89f254a0fc5f508191d2e
Jayon Kim <jpkim921@gmail.com>
2018-11-06 03:18:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function produceDrivingRange(blockRange){
   return function(startPoint, endPoint){
     let distance = Math.abs(parseInt(endPoint) - parseInt(startPoint));
-    console.log(distance)
 
     let difference = distance - blockRange
+    console.log(difference
+    )
 
   };
 }
