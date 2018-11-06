418039f62bfceceb70e1334ff96ccbe534a5ab9c
Jayon Kim <jpkim921@gmail.com>
2018-11-06 02:59:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function produceDrivingRange(blockRange){
   return function(startPoint, endPoint){
     let distance = parseInt(endPoint) - parseInt(startPoint);
     let range = Math.abs(distance)
+    console.log(blockRange)
     console.log(range)
     console.log(startPoint, endPoint)
 
